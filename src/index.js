885c00e44e00e71d5707ba382568663ac4d23da6
annettabel87 <annettabel@rambler.ru>
2021-08-15 02:12:58 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
-  
+    const first = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eigth', 'nine', 'ten', 'eleven', 'tvelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dec = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number > 0 && number < 20) {
+        return (first[number])
+    } else if (number >=20 && number < 100) {
+        if (number % 10 === 0) 
+          return (dec[Math.floor(number / 10) - 2]) 
+        else (number % 10 != 0) 
+          return (dec[Math.floor(number / 10) - 2] + ' ' + first[number % 10])        
+      }
+    else if (number >99 && number <1000) {
+        if ((number % 100) - ((number % 100)%10) === 0 && (number % 100) === 0)
+            return (first[Math.floor(number / 100)] + " hundred")
+        else return (first[Math.floor(number / 100)] + " hundred " + toReadable(number %100)) }      
 }
